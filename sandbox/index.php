6886efc63909835f88207e23d588b20794dfcfb4
Victoria Gibbs <victoriahgibbs@gmail.com>
2024-08-28 14:29:49 -0400

    work during class and examples

---
+++ sandbox/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sandbox</title>
+</head>
+
+<body>
+  <h1>Sandbox</h1>
+  <nav>
+    <ul>
+      <li><a href="oai.php">Object Access Identifier</a></li>
+      <li><a href="instrument.php">Instrument</a></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </nav>
+</body>
+
+</html>
