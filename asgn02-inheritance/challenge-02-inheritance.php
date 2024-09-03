f2d81b28571b7b5bc4876a90dae1a9ce0ac41d65
Victoria Gibbs <victoriahgibbs@gmail.com>
2024-09-03 16:54:15 -0400

    Starting asgn02-inheritance

---
+++ asgn02-inheritance/challenge-02-inheritance.php
@@ -0,0 +1,9 @@
+<!--
+-+Choose own category
+-+Pick topic that interests you
+-+Look for subcategories with similarities and shared attributes.
+-+Map inheritance on paper, list properties and methods.
+-Once on paper, you can start to code and define your classes. 
+-Inherit, extend, and override properties and methods. 
+-Create instances to test.
+-->
