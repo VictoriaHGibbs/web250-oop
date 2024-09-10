4de800fef64edf4959e00e01346679336e88bfc5
Victoria Gibbs <victoriahgibbs@gmail.com>
2024-09-10 18:44:09 -0400

    Completed the static challenge

--- asgn03/static-challenge/challenge04.php
+++ asgn03/static-challenge/challenge04.php
@@ -1,19 +1,19 @@
 <!--
-  - Reuse Bicycle and Unicycle classes from challenge 3
-  - Add a static property $instanceCount (like the student counter)
-  - Write a static method called create()  "add one to $instanceCount and will create and return a new instance of the class to us."
-  - Set visibility modifiers on $instanceCount and create()
+  -/ Reuse Bicycle and Unicycle classes from challenge 3
+  -/ Add a static property $instanceCount (like the student counter)
+  -/ Write a static method called create()  "add one to $instanceCount and will create and return a new instance of the class to us."
+  -/ Set visibility modifiers on $instanceCount and create()
   part 2
-  - define a constant for storing an array of categories ['Road', 'Mountain', 'Hybrid', 'Cruiser', 'City', 'BMX']
-  - set the const to public
-  - Add a $category property for instances to store their category
+  -/ define a constant for storing an array of categories ['Road', 'Mountain', 'Hybrid', 'Cruiser', 'City', 'BMX']
+  -/ set the const to public
+  -/ Add a $category property for instances to store their category
   part 3
-  - make $wheels a static property
-  - make wheelDetails() a static method
+  -/ make $wheels a static property
+  -/ make wheelDetails() a static method
   - Ensure that Unicycle returns expected wheelDetails()
   extra credit
-  - create a method in Unicycle that extends a method in Bicycle by executing code before and after it. (like Chef example)
-  Create a method in Unicycle which overrides a method in Bicycle but then also falls back to the original method if a condition is not met. 
+  - create a method in Unicycle that extends a method in Bicycle by executing code before and after it. (Chef example)
+  Create a method in Unicycle which overrides a method in Bicycle but then also falls back to the original method if a condition is not met. (Image example)
 -->
 
 <?php
@@ -25,7 +25,21 @@ class Bicycle
   public $year;
   public $description;
   private $weightKg = 0;
-  protected $wheels = 2;
+  protected static $wheels = 2;
+  public $category;
+
+  public const CATEGORIES = ['Road', 'Mountain', 'Hybrid', 'Cruiser', 'City', 'BMX'];
+
+  public static $instanceCount = 0;
+
+  public static function create()
+  {
+    $class = get_called_class();
+    $vehicleName = new $class;
+
+    self::$instanceCount++;
+    return $vehicleName;
+  }
 
   public function name()
   {
@@ -66,9 +80,15 @@ class Bicycle
     }
   }
 
-  public function wheelDetails()
+  // public function wheelDetails()
+  // {
+  //   return "It has " . $this->wheels . " wheel(s)";
+  // }
+
+  public static function wheelDetails()
   {
-    return "It has " . $this->wheels . " wheel(s)";
+    $wheelString = static::$wheels == 1 ? "1 wheel" : static::$wheels . " wheels";
+    return "It has " . $wheelString . ".";
   }
 
   public function setWeightKg($value)
@@ -79,5 +99,22 @@ class Bicycle
 
 class Unicycle extends Bicycle
 {
-  protected $wheels = 1;
+  protected static $wheels = 1;
 }
+
+echo 'Bicycle count: ' . Bicycle::$instanceCount . '<br>';
+echo 'Unicycle count: ' . Unicycle::$instanceCount . '<br>';
+
+$bikeyBike = Bicycle::create();
+$uniWheelie = Unicycle::create();
+
+echo 'After count increase. <br>';
+echo 'Bicycle count: ' . Bicycle::$instanceCount . '<br>';
+echo 'Unicycle count: ' . Unicycle::$instanceCount . '<br>';
+
+echo 'Categories: ' . implode(', ', Bicycle::CATEGORIES) . '<br>';
+$bikeyBike->category = Bicycle::CATEGORIES[0];
+echo 'Category: ' . $bikeyBike->category . '<br>';
+
+echo "Bicycle: " . Bicycle::wheelDetails() . "<br />";
+echo "Unicycle: " . Unicycle::wheelDetails() . "<br />";
