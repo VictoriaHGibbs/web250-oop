d5f754c8620e18de62bd1089a75d6ac18c31709f
Victoria Gibbs <victoriahgibbs@gmail.com>
2024-09-11 00:52:01 -0400

    worked on bird php page and about to work on index page

--- asgn03/Bird.php
+++ asgn03/Bird.php
@@ -1,31 +1,43 @@
 <?php
 
-class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+class Bird
+{
+  public $habitat;
+  public $food;
+  public $nesting = "tree";
+  public $conservation;
+  public $song = "chirp";
+  public $flying = "yes";
 
-    function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
-        return  $flying_string ;
-    }
+  public static $eggNum = 0;
+
+  public $instanceCount;
+
+  public static function create()
+  {
+    $class = get_called_class();
+    $birdClass = new $class;
+    return $birdClass;
+  }
+
+  public function canFly()
+  {
+    $flyingString = ($this->flying == "yes") ? "can fly" : "is stuck on the ground";
+    return $flyingString;
+  }
 }
 
-class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+class YellowBelliedFlyCatcher extends Bird
+{
+  public $name = "yellow-bellied flycatcher";
+  public $diet = "mostly insects.";
+  public $song = "flat chilk";
+  public static $eggNum = "3-4, sometimes 5.";
 }
 
-class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+class Kiwi extends Bird
+{
+  public $name = "kiwi";
+  public $diet = "omnivorous";
+  public $flying = "no";
 }
