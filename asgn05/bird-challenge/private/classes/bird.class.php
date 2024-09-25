8f00a6672fa08b7430d4376d8bd24fde805df894
Victoria Gibbs <victoriahgibbs@gmail.com>
2024-09-25 00:56:46 -0400

    Finished with chapter 7 bird challenge

--- asgn05/bird-challenge/private/classes/bird.class.php
+++ asgn05/bird-challenge/private/classes/bird.class.php
@@ -1,42 +1,61 @@
 <?php
 
-class Bird {
+class Bird
+{
 
-/*
+  /*
 Use the wnc-birds.csv file to create the properties
 Make all of the properties public.
 */
+  public $common_name;
+  public $habitat;
+  public $food;
+  public $nest_placement;
+  public $behavior;
+  public $conservation_id;
+  public $backyard_tips;
 
 
- 
   /*
   Create a protected constant array called CONSERVATION_OPTIONS using the following scale.
   Use the CONDITION_OPTIONS from the bicycle.class.php file
-
-  1 = Low concern
-  2 = Moderate concern
-  3 = Extreme concern
-  4 = Extinct
   */
 
- 
+  protected const CONSERVATION_OPTIONS = [
+    1 => 'Low concern',
+    2 => 'Moderate concern',
+    3 => 'Extreme concern',
+    4 => 'Extinct'
+  ];
 
- /*
+
+  /*
    - Create a public __contruct that accepts a list of $args[]
    - Use the Null coalescing operator
    - Create a default value of 1 for conservation_id
  */
+  public function __construct($args = [])
+  {
+    $this->common_name = $args['common_name'] ?? NULL;
+    $this->habitat = $args['habitat'] ?? NULL;
+    $this->food = $args['food'] ?? NULL;
+    $this->nest_placement = $args['nest_placement'] ?? NULL;
+    $this->behavior = $args['behavior'] ?? NULL;
+    $this->conservation_id = $args['conservation_id'] ?? 1;
+    $this->backyard_tips = $args['backyard_tips'] ?? NULL;
+  }
 
-
-
-/*
+  /*
   Create a public method called conservation(). This method should mimic the
     public function condition() from the bicycle.class.php file
+  */
 
-
-*/
-
-
+  public function conservation()
+  {
+    if ($this->conservation_id > 0) {
+      return self::CONSERVATION_OPTIONS[$this->conservation_id];
+    } else {
+      return "Unknown";
+    }
+  }
 }
-
-?>
--- asgn05/bird-challenge/private/shared/public_header.php
+++ asgn05/bird-challenge/private/shared/public_header.php
@@ -1,17 +1,31 @@
 <!doctype html>
 
 <html lang="en">
-  <head>
-    <title>WNC Birds <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
-    <meta charset="utf-8">
-  </head>
 
-  <body>
+<head>
+  <title>WNC Birds <?php if (isset($page_title)) {
+                      echo '- ' . h($page_title);
+                    } ?></title>
+  <meta charset="utf-8">
+  <style>
+    th,
+    td {
+      padding: 5px;
+    }
 
-    <header>
-      <h1>
-        <a href="<?php echo url_for('/index.php'); ?>">
-          WNC Birds
-        </a>
-      </h1>
-    </header>
+    th {
+      background-color: greenyellow;
+      text-transform: uppercase;
+    }
+  </style>
+</head>
+
+<body>
+
+  <header>
+    <h1>
+      <a href="<?php echo url_for('/index.php'); ?>">
+        WNC Birds
+      </a>
+    </h1>
+  </header>
